b1dc2c820cee9fdd8a0e2267fbc3c59b519c8ed3
Assaf Lavie <me@assaflavie.com>
2017-09-10 16:53:00 +0300

    don't pollute local dir

--- ubuntu/terraform/0.10.3.sh
+++ ubuntu/terraform/0.10.3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 set -eu
 
-curl -s -o terra.zip https://releases.hashicorp.com/terraform/0.10.3/terraform_0.10.3_linux_amd64.zip
+export TGT=(mktemp -d)
+curl -s -o $TGT/terra.zip https://releases.hashicorp.com/terraform/0.10.3/terraform_0.10.3_linux_amd64.zip
+cd $TGT
 unzip terra.zip
 mv terraform /usr/local/bin/
-rm terra.zip
+rm -rf $TGT
 
 
 
