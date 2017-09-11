f4c23955538d69d985f61c944cfdf243fe6e9490
Assaf Lavie <me@assaflavie.com>
2017-09-11 11:20:46 +0300

    typo

--- ubuntu/terraform/0.10.3.sh
+++ ubuntu/terraform/0.10.3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 set -eu
 
-export TGT=(mktemp -d)
+export TGT=$(mktemp -d)
 curl -s -o $TGT/terra.zip https://releases.hashicorp.com/terraform/0.10.3/terraform_0.10.3_linux_amd64.zip
 cd $TGT
 unzip terra.zip
