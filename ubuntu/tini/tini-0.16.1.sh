0059c4f0c406eb2bc53ecfba70d4c27514c791af
Assaf Lavie <me@assaflavie.com>
2018-02-06 09:40:26 +0200

    Fixed and renamed to invalidate cache

--- ubuntu/tini/tini-0.16.1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-RUN curl -sL -o /tini https://github.com/krallin/tini/releases/download/v0.16.1/tini
-RUN chmod +x /tini
-
---
+++ ubuntu/tini/tini-v0.16.1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -sL -o /tini https://github.com/krallin/tini/releases/download/v0.16.1/tini
+chmod +x /tini
+
