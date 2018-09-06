2af335e01c865b73c35b6147fc315ddd02f26fa6
Assaf Lavie <me@assaflavie.com>
2018-09-06 11:03:20 +0000

    begone erroneous file

--- ubuntu/gradle/4.10.sh
+++
@@ -1,6 +0,0 @@
-RUN cd $(mktemp -d) && \
-    curl -sL -o ./gradle.zip https://services.gradle.org/distributions/gradle-4.10-bin.zip && \
-    mkdir -p /opt/gradle && \
-    unzip gradle.zip -d /opt/gradle && \
-    rm gradle.zip && \
-    ln -s /opt/gradle/gradle-4.10/bin/gradle /usr/bin/gradle
